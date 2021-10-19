c690d17fae4518554c8612625abee01d4db7780b
anil9t5 <anilhitang@Anils-MacBook-Pro.local>
2021-10-18 22:32:25 -0300

    changed routing params in next config file

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ module.exports = {
       "/contact": { page: "/contact" },
       "/search": { page: "/search" },
       "/login": { page: "/login" },
+      "/plants/:id": { page: "/plants" },
     }
   },
   trailingSlash: true,
