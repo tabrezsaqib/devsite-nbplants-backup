648f38b867952ddc02b1cf86da15a081290ab771
anil9t5 <anilhitang@Anils-MacBook-Pro.local>
2023-01-10 02:16:33 -0400

    Updated admin data post api url and fixed type value in routing

--- redux/actions/postFileDataAction.js
+++ redux/actions/postFileDataAction.js
@@ -1,6 +1,6 @@
 import * as types from "../types"
 import * as api from "../../generics/api"
-const API_URL = process.env.API_URL
+const API_POST_URL = process.env.API_POST_URL
 
 // export const postFileData = (data) => async (dispatch) => {
 //   const response = await api.post(`${API_URL}plants`, data)
@@ -11,8 +11,8 @@ const API_URL = process.env.API_URL
 //   })
 // }
 export const postFileData = (data) => async (dispatch) => {
-  const response = await api.post(`${API_URL}plants_db`, data)
-  console.log(response)
+  const response = await api.post(`${API_POST_URL}plants_db`, data)
+
   dispatch({
     type: types.POST_JSONDATA,
     payload: response,
