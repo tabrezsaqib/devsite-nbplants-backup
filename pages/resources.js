1cf8e7c1eefb1fedd338f2b1625f7e4c3ad6128d
plantsnb <plantsnb@gmail.com>
2023-08-11 14:26:59 -0300

    Resources Static Page Changed

--- pages/resources.js
+++ pages/resources.js
@@ -1,368 +1,54 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const about = () => {
+
+const resources = () => {
   return (
     <div>
-      <section className="banner d-flex justify-content-center align-items-center">
-        <h2>Resources</h2>
-      </section>
-      <section>
-        <div className="row">
-          <div className="col-lg-2"></div>
-          <div className="col-lg-8">
-            <div className="content">
-              <h3>Resources</h3>
-              <p>
-                Boland, Todd. 2011.{" "}
-                <em>Trees and Shrubs, Newfoundland and Labrador</em>. Portugal
-                Cove-St. Philip’s, NL: Boulder Publications.
-              </p>
-              <p>
-                Boland, Todd. 2012. <em>Trees and Shrubs of the Maritimes</em>.
-                Portugal Cove-St. Philip’s, NL: Boulder Publications.
-              </p>
-              <p>
-                Boland, Todd. 2015. <em>Wildflowers of New Brunswick</em>.
-                Portugal Cove-St. Philip’s, NL: Boulder Publications.
-              </p>
-              <p>
-                Campbell, Christopher S. and Hyland, Fay (botanists) and
-                Campbell, Mary L.F. (illustrator). 1975.{" "}
-                <em>Winter Keys to Woody Plants of Maine</em> (Rev. ed.). Orono,
-                ME: University of Maine Press.
-              </p>
-              <p>
-                Chadde, Steve W. 2019.{" "}
-                <em>
-                  Northeast Ferns: A Field Guide to the Ferns and Fern Relatives
-                  of the Northeastern United States
-                </em>
-                . Orchard Innovations.
-              </p>
-              <p>
-                Cobb, Boughton and Elizabeth Farnsworth and Cheryl Lowe for the
-                New England Wild Flower Society. 2005.
-                <em>
-                  A Field Guide to Ferns and Their Related Families:
-                  Northeastern and Central North America
-                </em>{" "}
-                (2nd ed.). Boston: Houghton Mifflin.
-              </p>
-              <p>
-                Cody, William J. and Britton, Donald M. 1989.{" "}
-                <em>Ferns and Fern Allies of Canada</em>. Research Branch,
-                Agriculture Canada Publication 1829/E
-              </p>
-              <p>
-                Elliman, Ted & New England Wild Flower Society. 2016.{" "}
-                <em>Wildflowers of New England</em>. Portland, OR: Timber Press.
-              </p>
-              <p>
-                Fernald, Merritt Lyndon. 1950{" "}
-                <em>
-                  Gray’s Manual of Botany: A handbook of the flowering plants
-                  and ferns of the central and northeastern United States and
-                  adjacent Canada
-                </em>{" "}
-                (8th (centennial) ed.). Portland, Oregon: Dioscorides Press.
-              </p>
-              <p>
-                Gleason, Henry A. and Cronquist, Arthur. 1991.{" "}
-                <em>
-                  Manual of Vascular Plants of Northeastern United States and
-                  Adjacent Canada
-                </em>{" "}
-                (2nd ed.). Bronx, NY: The New York Botanical Garden.
-              </p>
-              <p>
-                Haines, Arthur. Illustrated by Elizabeth Farnsworth and Gordon
-                Morrison. 2011.
-                <em>
-                  New England Wild FLower Society’s Flora Novae Angliae: A
-                  Manual for the Identification of Native and Naturalized Higher
-                  Vascular Plants of New England
-                </em>
-                . New Haven, CT: New England Wildflower Society and Yale
-                University Press.
-              </p>
-              <p>
-                Harlow, William M. 1941.{" "}
-                <em>Fruit Key and Twig Key to Trees and Shrubs</em>. New York:
-                Dover Publications.
-              </p>
-              <p>
-                Hinds, Harold R. 2000. <em>Flora of New Brunswick</em> (2nd
-                ed.). Fredericton, NB: Robinson & Greenwood.
-              </p>
-              <p>
-                Hinds, Harold R. 1983.{" "}
-                <em>The Rare Vascular Plants of New Brunswick</em>. Fredericton,
-                NB: Connell Memorial Herbarium.
-              </p>
-              <p>
-                Maine Forest Service, Department of Agriculture , Conservation
-                and Forestry. 2008. <em>Forest Trees of Maine</em> (Centennial
-                ed.).
-              </p>
-              <p>
-                Moyle, John B. and Evelyn W. Moyle. 1977.{" "}
-                <em>
-                  Northland Wild Flowers: A Guide for the Minnesota Region
-                </em>
-                . Minneapolis, MN: University of Minnesota Press.
-              </p>
-              <p>
-                Munden, Carl. 2021, 2001.{" "}
-                <em>Native Orchids of Nova Scotia: A Field Guide</em>. Sydney,
-                NS: Breton Books.
-              </p>
-              <p>
-                Palmer, David and Glynn, Tracy. 2019.{" "}
-                <em>The Great Trees of New Brunswick</em> (2nd ed.).
-                Fredericton, NB: Goose Lane Editions.
-              </p>
-              <p>
-                Patel, Nikisha R.; Fawcett, Susan; and Gilman, Arthur V. 2019.
-                Phegopteris excelsior (Thelypteridaceae): A New Species of North
-                American Tetraploid Beech Fern, <em>Novon</em> 27: 211-218.
-              </p>
-              <p>
-                Scoggan, H. J. 1978. <em>The Flora of Canada</em>. Ottawa, ON:
-                National Museums of Canada.
-              </p>
-              <p>
-                Sibley, David Allen. 2009. <em>The Sibley Guide to Trees</em>.
-                New York: Alfred A. Knopf.
-              </p>
-              <p>
-                Soper, James H. and Heimburger, Margaret L. 1982.{" "}
-                <em>Shrubs of Ontario</em>. Toronto: The Royal Ontario Museum.
-              </p>
-              <p>
-                Voss, Edward G. and Reznicek, Anton A. 2012.{" "}
-                <em>Field Manual of Michigan Flora</em>. Ann Arbor: The
-                University of Michigan Press.
-              </p>
-              <p>
-                Wherry, Edgar T. 1995.{" "}
-                <em>
-                  The Fern Guide: Northeastern and Midland United States and
-                  Adjacent Canada
-                </em>
-                . New York: Dover Publications.
-              </p>
-              <p>
-                Wojtech, Michael. 2011.{" "}
-                <em>Bark: A Field Guide to Trees of the Northeast</em>. Waltham,
-                MA: Brandeis University Press.
-              </p>
-              <p>
-                Young, C. Mary. 2015.{" "}
-                <em>
-                  Nature’s Bounty: Four Centuries of Plant Exploration in New
-                  Brunswick
-                </em>
-                . Fredericton, NB: UNB Libraries, University of New Brunswick.
-              </p>
-              <p>
-                Zinck, Marian. 1998.{" "}
-                <em>Roland’s Flora of Nova Scotia, Vol 1</em>. NS: Nova Scotia
-                Museum and Nimbus Publishing.
-              </p>
-              <p>
-                Zinck, Marian. 1998.{" "}
-                <em>Roland’s Flora of Nova Scotia, Vol 2</em>. NS: Nova Scotia
-                Museum and Nimbus Publishing.
-              </p>
-
-              <h3>
-                <strong>
-                  {" "}
-                  <i className="bi bi-link-45deg"></i>&nbsp;Web Resources{" "}
-                </strong>
-              </h3>
-              <p>
-                <a
-                  href="http://accdc.com//"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Atlantic Canada Conservation Data Centre
-                </a>
-              </p>
-              <p>
-                <a
-                  href="http://www.canadensys.net/"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Canadensys
-                </a>
-              </p>
-              <p>
-                <a
-                  href="https://unbherbarium.lib.unb.ca/page/connell-memorial-herbarium"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Connell Memorial Herbarium, University of New Brunswick
-                </a>
-              </p>
-              <p>
-                <a
-                  href="http://www.efloras.org/flora_page.aspx?flora_id=1"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Flora of North America
-                </a>
-                , eFloras.org
-              </p>
-              <p>
-                <a
-                  href="https://www.minnesotawildflowers.info/"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Minnesota Wildflowers
-                </a>
-                , Katy Chayka
-              </p>
-              <p>
-                <a
-                  href="https://gobotany.nativeplanttrust.org/"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Go Botany
-                </a>
-                , Native Plant Society
-              </p>
-              <p>
-                <a
-                  href="https://goorchids.northamericanorchidcenter.org/"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Go Orchids
-                </a>
-                , North American Orchid Conservation Center
-              </p>
-              <p>
-                <a
-                  href="http://www.illinoiswildflowers.info"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Illinois Wildflowers
-                </a>
-                , Hilty John.
-              </p>
-              <p>
-                <a
-                  href="http://www.accdc.com/peiflora/s1.htm"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Illustrated Flora of Prince Edward Island
-                </a>
-                , Atlantic Canada Conservation Data Centre
-              </p>
-              <p>
-                <a
-                  href="https://www.herbarium.iastate.edu/moonwort-botrychium-systematics"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Moonwort (Botrychium) Systematics
-                </a>
-                , Ada Hayden Herbarium, Iowa State University
-              </p>
-              <p>
-                <a
-                  href="https://www.maine.gov/dacf/mnap/features/rare_plants/index.htm"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Rare Plants in Maine
-                </a>{" "}
-                and{" "}
-                <a
-                  href="https://www.maine.gov/dacf/mnap/features/invasive_plants/invasives.htm"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Invasive Plants in Maine
-                </a>
-                , Maine Natural Areas Program, Maine Department of Agriculture,
-                Conservation & Forestry
-              </p>
-              <p>
-                <a
-                  href="http://www.missouribotanicalgarden.org/plantfinder/plantfindersearch.aspx"
-                  className="links"
-                  target="_blank"
-                  rel="noreferrer">
-                  Missouri Botanic Garden Plant Finder
-                </a>
-              </p>
-
-              <h3>Personal Communication</h3>
-              <p>Donald Farrar with Jim Goltz, 2023.</p>
-              <p>Art Gilman with Jim Goltz, 2023.</p>
-            </div>
-          </div>
-          <div className="col-lg-2"></div>
-        </div>
-      </section>
-
-      <style jsx>{`
-        section {
-          padding: 20px 10px;
-          margin: 40px 20px;
-          margin-bottom: 50px;
-          h2 {
-            font-weight: 900;
-          }
+      <div className="resourcesPageContainer">
+        <iframe 
+        className="resourcesPageContent"
+        src="https://api-v2.newbrunswickplants.ca/resources/">
+        </iframe>
+      </div>
+      <style jsx>
+      {`
+        .resourcesPageContainer{
+         position: relative;
+         overflow:hidden;
+         margin:0;
+         padding-top: 100vh;
         }
-        .banner {
-          background-image: url("../images/viola-adunca-fl-ahaines-b.jpeg");
-          box-shadow: inset 0 0 0 2000px rgba(53, 61, 61, 0.4);
-          background-position: center;
-          background-size: cover;
-          height: 140px;
-          margin: 30px 0px;
-          h2 {
-            color: #ffffff;
-            font-weight: 900;
-          }
-        }
-        .img-container {
-          overflow: hidden;
+        .resourcesPageContent{
+          position: absolute;
+          top:0;
+          border: 0;
+          margin:0;
+          left:0;
           width: 100%;
-          height: 300px;
-          border-radius: 12px;
-          border: 1px solid #e0e1e3;
-          img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
+          height:100%;
+        }
+        @media screen and (min-width: 1200px){
+          .resourcesPageContainer{
+           }
+           .resourcesPageContent{
+             position: absolute;
+             width: 100vw;
+             height: 100vh;
+             box-sizing:border-box;
+             top:0;
+             border: 0;
+             margin:0;
+             left:0;
+           }
           }
-        }
-        .team-profile {
-          padding: 15px;
-          h4 {
-            font-size: 18px;
-            font-weight: 900;
-          }
-        }
-        .links {
-          color: #1d9d47;
-        }
-      `}</style>
+      `}
+    </style>
     </div>
   )
 }
 
-export default about
+export default resources;
+
+
