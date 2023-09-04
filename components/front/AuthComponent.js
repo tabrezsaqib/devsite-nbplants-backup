fabc64c6e21689b3dc773256f317dd8bec84e52e
plantsnb <plantsnb@gmail.com>
2023-09-04 15:49:10 -0300

    CSS Changes Front Page

--- components/front/AuthComponent.js
+++ components/front/AuthComponent.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import {useRouter} from 'next/router';
 import { connect, useDispatch } from "react-redux"
 import { authenticateAction } from '../../redux/actions/authenticateAction';
+import styles from "../../styles/AuthComponent.module.css"
 
 const AuthComponent = ({children, authentication_state}) => {
   const dispatch = useDispatch();
@@ -24,14 +25,14 @@ const AuthComponent = ({children, authentication_state}) => {
     if (!authentication_state){
         return(
             <>
-            <div className="loginContainer row">
-                <h2 className="loginTitle text-center mt-4">NB Plants Login Page</h2>
-                <div className="form-section">
+            <div className={[styles.loginContainer, "row"].join(" ")}>
+                <h2 className={[styles.loginTitle, "text-center", "mt-4"].join(" ")}>NB Plants Login Page</h2>
+                <div className={[styles.formSection, "form-section"].join(" ")}>
                   <form onSubmit={handleLogin}>
                     <div className="form-group">
                       <input
                         type="text"
-                        className="form-control mb-3"
+                        className={[styles.formControl, "form-control", "mb-3"].join(" ")}
                         aria-describedby="username"
                         placeholder="Username"
                         value={userId}
@@ -41,81 +42,18 @@ const AuthComponent = ({children, authentication_state}) => {
                     <div className="form-group">
                       <input
                         type="password"
-                        className="form-control mb-3"
+                        className={[styles.formControl, "form-control", "mb-3"].join(" ")}
                         placeholder="Password"
                         value={password}
                 onChange={(e) => setPassword(e.target.value)}
                       />
                     </div>
-                    <button type="submit" className="submitBtn btn btn-success">
+                    <button type="submit" className={["submitBtn", styles.btn, styles.btnSuccess, "btn", "btn-success"].join(" ")}>
                       Submit
                     </button>
                   </form>
                 </div>
               </div>
-      
-            <style jsx>{`
-            .loginContainer{
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              justify-content: center;
-            }
-            .loginTitle{
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              font-size: 32px;
-              font-weight: bold;
-              margin: 24px 0px 8px;
-              color: #333333;
-              text-align: center;
-              margin-bottom: 0.5rem;
-              font-weight: 500;
-              line-height: 1.2rem;
-            }
-              .form-section {
-              display: flex;
-              margin-top: 60px;
-              padding: 30px;
-              border: 1px solid rgb(224, 225, 227);
-              margin-bottom: 60px;
-              }
-              .form-control{
-                margin-bottom: 1rem !important;
-                display: flex;
-                width: 100%;
-                padding: 0.375rem 0.75rem;
-                font-size: 1rem;
-                font-weight: 400;
-                line-height: 1.5;
-                background-clip: padding-box;
-                border: 1px solid #ced4da;
-                appearance: none;
-                border-radius: 0.25rem;
-                transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-              }
-              .btn, .btn-success{
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                color: #fff;
-                background-color: #198754;
-                border-color: #198754;
-                font-weight: 400;
-                line-height: 1.5;
-                text-align: center;
-                text-decoration: none;
-                vertical-align: middle;
-                user-select: none;
-                border: 1px solid transparent;
-                padding: 0.375rem 0.75rem;
-                font-size: 1rem;
-                border-radius: 0.25rem;
-                cursor: pointer;
-                transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-              }
-            `}</style>
           </>
         );
     }
