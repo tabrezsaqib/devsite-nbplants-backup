e684e07512f5ba3098c16e029df4c2dd5c0ecd93
plantsnb <plantsnb@gmail.com>
2024-02-11 10:49:06 -0400

    Modified Search Result

--- components/search/SearchItem.js
+++ components/search/SearchItem.js
@@ -15,47 +15,47 @@ const SearchItem = ({ plant }) => {
                 ? plant.acf.gallery_view_image.url
                 : "../images/no_result_found.png"
             }
-            alt="" onContextMenu={(e)=>e.preventDefault()}
+            alt="" onContextMenu={(e) => e.preventDefault()}
           />
         </div>
         <div className="description">
-          <h4 className={[styles.heading, "heading mt-3"].join(" ")}>{plant.title.rendered}</h4>
+          <h4 className={[styles.heading, "heading mt-3"].join(" ")}>{plant.title.rendered ? plant.title.rendered : plant.title}</h4>
           <div className="d-flex flex-column">
             <span>
               <strong>Common name:</strong> {plant.acf.common_name}
             </span>
             <div className={["d-flex", "flex-wrap", styles.tags, "tags"].join(" ")}>
-            {plant.acf.characteristics === ''? <>{
-              plant.acf.characteristics.habitat.map((val, index) => (
-                <span className={styles.spanContent} key={index}>
-                  {api.capitalizeFirstLetter(val)}, &nbsp;
-                </span>
-              ))}
-              {plant.acf.characteristics.leaf_type.map((val, index) => (
-                <span className={styles.spanContent} key={index}>
-                  {api.capitalizeFirstLetter(val)}, &nbsp;
-                </span>
-              ))}
-              {plant.acf.characteristics.leaf_arrangement.map((val, index) => (
-                <span className={styles.spanContent} key={index}>
-                  {api.capitalizeFirstLetter(val)}, &nbsp;
-                </span>
-              ))}
-              {plant.acf.characteristics.leaf_blade_edges.map((val, index) => (
-                <span className={styles.spanContent} key={index}>
-                  {api.capitalizeFirstLetter(val)}, &nbsp;
-                </span>
-              ))}
-              {plant.acf.characteristics.flower_colour.map((val, index) => (
-                <span className={styles.spanContent} key={index}>
-                  {api.capitalizeFirstLetter(val)}, &nbsp;
-                  {val !==
-                  plant.acf.characteristics.flower_colour.slice(-1).pop()
-                    ? ","
-                    : ""}
-                  &nbsp;
-                </span>
-              ))}</>:''}
+              {plant.acf.characteristics === '' ? <>{
+                plant.acf.characteristics.habitat.map((val, index) => (
+                  <span className={styles.spanContent} key={index}>
+                    {api.capitalizeFirstLetter(val)}, &nbsp;
+                  </span>
+                ))}
+                {plant.acf.characteristics.leaf_type.map((val, index) => (
+                  <span className={styles.spanContent} key={index}>
+                    {api.capitalizeFirstLetter(val)}, &nbsp;
+                  </span>
+                ))}
+                {plant.acf.characteristics.leaf_arrangement.map((val, index) => (
+                  <span className={styles.spanContent} key={index}>
+                    {api.capitalizeFirstLetter(val)}, &nbsp;
+                  </span>
+                ))}
+                {plant.acf.characteristics.leaf_blade_edges.map((val, index) => (
+                  <span className={styles.spanContent} key={index}>
+                    {api.capitalizeFirstLetter(val)}, &nbsp;
+                  </span>
+                ))}
+                {plant.acf.characteristics.flower_colour.map((val, index) => (
+                  <span className={styles.spanContent} key={index}>
+                    {api.capitalizeFirstLetter(val)}, &nbsp;
+                    {val !==
+                      plant.acf.characteristics.flower_colour.slice(-1).pop()
+                      ? ","
+                      : ""}
+                    &nbsp;
+                  </span>
+                ))}</> : ''}
             </div>
           </div>
         </div>
