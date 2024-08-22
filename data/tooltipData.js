dd796cbc14e7337c31dd20d7a9a9e67f47d0693b
Plants NB <92542795+plantsnb@users.noreply.github.com>
2024-08-22 14:18:56 -0300

    Add files via upload

---
+++ data/tooltipData.js
@@ -0,0 +1,145 @@
+export const tooltipData = [{
+  heading: 'Flower Arrangement (Inflorescence)',
+  options: [{
+    value: 0,
+    content: "<span class='labelDesc'><b>Catkin: </b><span class='labelDesc'>dense spike or raceme of unisexual flowers; often pendulous and cylindrical, falling as a whole after fruiting</span></span>",
+    label: 'catkin'
+  },
+  {
+    value: 3,
+    content: "<span class='labelDesc'><b>Cyme: </b><span class='labelDesc'>flat-topped flower arrangement with stalked flower rising from one point of a common stalk; the inner flowers bloom first</span></span>",
+    label: 'cyme'
+  },
+  {
+    value: 4,
+    content: "<span class='labelDesc'><b>Panicle: </b><span class='labelDesc'>loose irregular arrangement of stalked flowers; each branch has multiple stalked flowers; a panicle includes multiple racemes</span></span>",
+    label: 'panicle'
+  },
+  {
+    value: 5,
+    content: "<span class='labelDesc'><b>Raceme: </b><span class='labelDesc'>flower arrangement where each flower is on an individual stalk along a central stem; a panicle includes multiple racemes</span></span> ",
+    label: 'raceme'
+  },
+  {
+    value: 7,
+    content: "<span class='labelDesc'><b>Solitary flower: </b><span class='labelDesc'>a single flower that is not part of a grouped flower arrangement</span></span>  <img src='/images/flower.png' style={{border:'1px solid grey'}} height='30' width='30'></img>",
+    label: 'solitary flower'
+  },
+  {
+    value: 6,
+    content: "<span class='labelDesc'><b>Spike: </b><span class='labelDesc'>stalkless (or nearly so) flowers arranged on an elongated stem</span></span>  <img src='/images/flower.png' style={{border:'1px solid grey'}} height='30' width='30'></img>",
+    label: 'spike'
+  },
+  {
+    value: 8,
+    content: "<span class='labelDesc'><b>Umbel: </b><span class='labelDesc'>flat-topped flower arrangement with stalked flowers rising from one point of a common stalk; the outer flowers open first</span></span>",
+    label: 'umbel'
+  }]
+},
+{
+  heading: 'Leaf Type',
+  options: [{
+    value: 0,
+    content: "<span class='labelDesc'><b>Compound: </b><span class='labelDesc'>composed of 2 or more leaflets arising from a non-woody stalk to form a single leaf</span></span>",
+    label: 'compound'
+  },
+  {
+    value: 1,
+    content: "<span class='labelDesc'><b>Needles/Scales: </b><span class='labelDesc'>Needles are long, narrow specialized leaves;  in NB, mostly found on evergreen trees. Scales are flat to slightly curved, reduced leaves; often overlapping</span></span>",
+    label: 'needles / scales'
+  },
+  {
+    value: 2,
+    content: "<span class='labelDesc'><b>Simple: </b><span class='labelDesc'>a single undivided leaf blade</span></span>",
+    label: 'simple'
+  },
+  {
+    value: 3,
+    content: "<span class='labelDesc'><b>No Leaves: </b></span> ",
+    label: 'no leaves'
+  }]
+},
+{
+  heading: 'Leaf Edge',
+  options: [{
+    value: 0,
+    content: "<span class='labelDesc'><b>Lobed: </b><span class='labelDesc'>deep indents on edges of leaf blades</span></span>",
+    label: 'lobed'
+  },
+  {
+    value: 2,
+    content: "<span class='labelDesc'><b>Toothed: </b><span class='labelDesc'>small indents/teeth on edges of leaf blades</span></span>",
+    label: 'toothed'
+  },
+  {
+    value: 1,
+    content: "<span class='labelDesc'><b>Smooth: </b><span class='labelDesc'>no teeth or lobes on edges of leaf blades</span></span>",
+    label: 'smooth'
+  }
+]
+},
+{
+  heading: 'Leaf Arrangement',
+  options: [{
+    value: 0,
+    content: "<span class='labelDesc'><b>Alternate: </b><span class='labelDesc'>Leaves connected one per node on a stem; includes when only one leaf on a stem</span></span>",
+    label: 'alternate'
+  },
+  {
+    value: 1,
+    content: "<span class='labelDesc'><b>Basal: </b><span class='labelDesc'>Leaves at ground level, at or near the base of the plant</span></span>",
+    label: 'basal'
+  },
+  {
+    value: 2,
+    content: "<span class='labelDesc'><b>Opposite: </b><span class='labelDesc'>Leaves arranged directly across each other, 2 or more per node on a stem; includes a whorled circle of leaves around the stem</span></span>",
+    label: 'opposite'
+  }
+]
+},
+{
+  heading: 'Petal Symmetry',
+  options: [{
+    value: 0,
+    content: "<span class='labelDesc'><b>Bilateral petal symmetry: </b><span class='labelDesc'>flower can only be divided evenly in one way e.g. Lady's Slipper Orchids</span></span>",
+    label: 'bilateral'
+  },
+  {
+    value: 1,
+    content: "<span class='labelDesc'><b>Radial petal symmetry: </b><span class='labelDesc'>flower can be divided evenly multiple ways e.g. Daisies</span></span>",
+    label: 'radial'
+  }]
+},
+{
+  heading: 'Fruit Type',
+  options: [{
+    value: 0,
+    content: "<span class='labelDesc'><b>Achene fruit: </b><span class='labelDesc'>a small, dry, one-seeded fruit that doesn’t open when mature</span></span>",
+    label: 'achene'
+  },
+  {
+    value: 1,
+    content: "<span class='labelDesc'><b>Berry: </b><span class='labelDesc'>fleshy fruit with 2 or more seeds that doesn’t open when mature</span></span>",
+    label: 'berry'
+  },
+  {
+    value: 2,
+    content: "<span class='labelDesc'><b>Capsule: </b><span class='labelDesc'>dry fruit that usually opens when mature. Seeds are within 2 or more chambers that open by valves or teeth</span></span>",
+    label: 'capsule'
+  },{
+    value: 7,
+    content: "<span class='labelDesc'><b>Nut: </b><span class='labelDesc'>fruit with a single locule (ovary chamber) and a single seed surrounded by a thick, hard pericarp, often supported by a whorl of bracts</span></span>",
+    label: 'nut'
+  },
+  {
+    value: 8,
+    content: "<span class='labelDesc'><b>Pod: </b><span class='labelDesc'>any dry fruit that opens when mature e.g. Pea family</span></span>",
+    label: 'pod'
+  },
+  {
+    value: 10,
+    content: "<span class='labelDesc'><b>Stone (Drupe): </b><span class='labelDesc'> fleshy fruit with think skin and a central stone containing the seed e.g. Plum family</span></span> ",
+    label: 'stone'
+  }]
+},
+]
